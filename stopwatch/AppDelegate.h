a069c9d915258e3b403d29d50202c1e2a60c26d8
Kevin Kelly <kevinpk@gmail.com>
2015-10-25 19:21:17 -0500

    Initial Commit

---
+++ stopwatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stopwatch
+//
+//  Created by Kevin Kelly on 10/25/15.
+//  Copyright © 2015 Kevin Kelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ stopwatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stopwatch
+//
+//  Created by Kevin Kelly on 10/25/15.
+//  Copyright © 2015 Kevin Kelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
